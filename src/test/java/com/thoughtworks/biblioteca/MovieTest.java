531f06960653deaa074a4ee9a79c65ce6a065fb5
natashaj7 <njimenez@thoughtworks.com>
2015-09-28 09:46:49 -0500

    listbooks command and started testing movies class

--- src/test/java/com/thoughtworks/biblioteca/MovieTest.java
+++ src/test/java/com/thoughtworks/biblioteca/MovieTest.java
@@ -1,12 +1,23 @@
 package com.thoughtworks.biblioteca;
 
 import org.junit.Test;
+import org.mockito.asm.ClassReader;
+
+import java.io.PrintStream;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 /**
  * Created by njimenez on 9/28/15.
  */
 public class MovieTest {
 
+    @Test
+    public void shouldDisplayDetailsToUserWhenOutputting() {
+        Movie movie = new Movie("movie1", "1993", "Natasha", "6");
+
+        assertEquals(movie.movieOutput(), "movie | 1993 | Natasha | 6");
+    }
 }
